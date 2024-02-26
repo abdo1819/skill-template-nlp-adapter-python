96ae0fc2a4a5fac025627e2ec7febb18f8515cbc
abdelrahman ragab <abdo14hashem15@gmail.com>
2024-02-26 17:02:18 +0200

    add a log for errors

--- app/app.py
+++ app/app.py
@@ -1,5 +1,19 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
 from .views import skill
+
+async def http422_error_handler(
+    _: Request, exc: Union[RequestValidationError, ValidationError]
+) -> JSONResponse:
+    print(await _.json())
+    print(exc.errors())
+    return JSONResponse(
+        {"errors": exc.errors()}, status_code=422
+    )
+
 
 def create_app():
   app = FastAPI(
@@ -10,5 +24,8 @@ def create_app():
 
   # Add routes
   app.include_router(router=skill.router)
+  app.add_exception_handler(ValidationError, http422_error_handler)
+  app.add_exception_handler(RequestValidationError, http422_error_handler)
+
 
   return app
